c39a32cc440ded5d74b72358b957eda5b3ef00e1
abc@bca.com <abc@bca.com>
2023-12-04 23:27:16 +0530

    --combined-reducers

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import { createStore, bindActionCreators } from "redux";
+import { createStore, bindActionCreators, combineReducers } from "redux";
 const CAKE_ORDERED = "sfgsdg";
 const CAKE_RESTOCKED = "sfgsdf";
+const ICECREAM_ORDERED = "cfjnv";
+const ICECREAM_RESTOCKED = "sljdfhn";
 // Action
 function orderCake() {
   return {
@@ -16,12 +18,30 @@ function cakeRestocked(qty = 1) {
   };
 }
 
-const initialState = {
+function iceCreamOrdered(qty = 1) {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: qty,
+  };
+}
+
+function iceCreamRestocked(qty = 1) {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: qty,
+  };
+}
+
+const cakeInitialState = {
   numOfCakes: 10,
   anotherThings: 0,
 };
+const iceCreamInitialState = {
+  numOfIceCream: 10,
+  anotherThings: 0,
+};
 // reducers : (prevState, action) => newState
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = cakeInitialState, action) => {
   switch (action.type) {
     case CAKE_ORDERED:
       return {
@@ -35,11 +55,35 @@ const reducer = (state = initialState, action) => {
       };
 
     default:
-      state;
+      return state;
   }
 };
 
-const store = createStore(reducer);
+const iceCreamReducer = (state = iceCreamInitialState, action) => {
+  switch (action.type) {
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numOfIceCream: state.numOfIceCream - action.payload,
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIceCream: state.numOfIceCream + action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+// both above reducers will  work independently
+const rootReducers = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+// store only accept one reducer
+const store = createStore(rootReducers);
 console.log("Initial State ", store.getState());
 
 const unsubcribe = store.subscribe(() =>
@@ -51,10 +95,16 @@ const unsubcribe = store.subscribe(() =>
 // store.dispatch(cakeRestocked(3));
 
 // we can bind the actions
-const action = bindActionCreators({ orderCake, cakeRestocked }, store.dispatch);
+const action = bindActionCreators(
+  { orderCake, cakeRestocked, iceCreamOrdered, iceCreamRestocked },
+  store.dispatch
+);
 action.orderCake();
 action.orderCake();
 action.orderCake();
+action.iceCreamOrdered(2);
+action.iceCreamRestocked(5);
 action.cakeRestocked(3);
 
 unsubcribe();
+console.log(store.getState().iceCream.numOfIceCream);
